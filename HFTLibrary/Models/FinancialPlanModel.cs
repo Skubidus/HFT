8f68e9305fc167130a8f054cdf8f6d5021bbcc74
Dennis Döring <big_atze@hotmail.com>
2024-11-11 13:13:54 +0100

    - Deleted SavingsPlanTypeEnum because it is not needed and was left behind. - Implemented IncomeEntryModel. - Marked some properties in FinancialPlanModel as required.

--- HFTLibrary/Models/FinancialPlanModel.cs
+++ HFTLibrary/Models/FinancialPlanModel.cs
@@ -3,12 +3,12 @@
 public class FinancialPlanModel
 {
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    public required string Name { get; set; }
     public string Description { get; set; } = string.Empty;
     public List<IncomeEntryModel> Incomes { get; init; } = [];
     public List<ExpenseEntryModel> Expenses { get; init; } = [];
     public List<BankAccountModel> BankAccounts { get; init; } = [];
     public SavingsPlanModel? SavingsPlan { get; set; } = null;
-    public DateTime DateCreated { get; set; }
-    public DateTime DateModified { get; set; }
+    public required DateTime DateCreated { get; set; }
+    public required DateTime DateModified { get; set; }
 }
--- HFTLibrary/Models/IncomeEntryModel.cs
+++ HFTLibrary/Models/IncomeEntryModel.cs
@@ -2,5 +2,9 @@
 
 public class IncomeEntryModel
 {
-    // TODO: implement IncomeEntryModel
+    public int Id { get; set; }
+    public required string Name { get; set; } = string.Empty;
+    public required decimal Price { get; set; }
+    public BankAccountModel? AssociatedBankAccount { get; set; }
+    public string Note { get; set; } = string.Empty;
 }
