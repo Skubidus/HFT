ddf9737c9f5ec89fb901d591135a3de067038542
Dennis Döring <big_atze@hotmail.com>
2024-11-11 13:21:21 +0100

    - Removed some default inittialization from IncomeEntryModel because the property was marked as required. - Renamed Price to TotalAmount in IncomeEntryModel. - Implemented ExpenseEntryModel.

--- HFTLibrary/Models/ExpenseEntryModel.cs
+++ HFTLibrary/Models/ExpenseEntryModel.cs
@@ -2,5 +2,9 @@
 
 public class ExpenseEntryModel
 {
-    // TODO: implement ExpenseEntryModel
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required decimal Price { get; set; }
+    public BankAccountModel? AssociatedBankAccount { get; set; }
+    public string Note { get; set; } = string.Empty;
 }
--- HFTLibrary/Models/IncomeEntryModel.cs
+++ HFTLibrary/Models/IncomeEntryModel.cs
@@ -3,8 +3,8 @@
 public class IncomeEntryModel
 {
     public int Id { get; set; }
-    public required string Name { get; set; } = string.Empty;
-    public required decimal Price { get; set; }
+    public required string Name { get; set; }
+    public required decimal TotalAmount { get; set; }
     public BankAccountModel? AssociatedBankAccount { get; set; }
     public string Note { get; set; } = string.Empty;
 }
