02af6bc5ea7753a377ecaad8d3fae5e95bb99e58
Dennis Döring <big_atze@hotmail.com>
2024-11-25 16:32:59 +0100

    - Included the bootstrap icon CDN. - Further implemented the EditFinancialPlan page. - Added a PriceString / TotalAmountString property to some models to be able to bind them in Blazor.

--- HFTLibrary/Models/IncomeEntryModel.cs
+++ HFTLibrary/Models/IncomeEntryModel.cs
@@ -1,11 +1,31 @@
-﻿namespace HFTLibrary.Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HFTLibrary.Models;
 
 public class IncomeEntryModel
 {
-	public int Id { get; set; }
-	public required string Name { get; set; }
-	public required decimal TotalAmount { get; set; }
-	public string Description { get; set; } = string.Empty;
-	public required DateTime DateCreated { get; set; }
-	public required DateTime DateModified { get; set; }
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required decimal TotalAmount { get; set; }
+    [NotMapped]
+    public string TotalAmountString
+    {
+        get
+        {
+            return TotalAmount.ToString("#,#0.00");
+        }
+
+        set
+        {
+            if (decimal.TryParse(value, out decimal amount) == false)
+            {
+                return;
+            }
+
+            TotalAmount = amount;
+        }
+    }
+    public string Description { get; set; } = string.Empty;
+    public required DateTime DateCreated { get; set; }
+    public required DateTime DateModified { get; set; }
 }
--- HFTLibrary/Models/SavingsEntryModel.cs
+++ HFTLibrary/Models/SavingsEntryModel.cs
@@ -1,11 +1,31 @@
-﻿namespace HFTLibrary.Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HFTLibrary.Models;
 
 public class SavingsEntryModel
 {
 	public int Id { get; set; }
 	public required string Name { get; set; }
 	public required decimal Price { get; set; }
-	public string Description { get; set; } = string.Empty;
+    [NotMapped]
+    public string PriceString
+    {
+        get
+        {
+            return Price.ToString("#,#0.00");
+        }
+
+        set
+        {
+            if (decimal.TryParse(value, out decimal price) == false)
+            {
+                return;
+            }
+
+            Price = price;
+        }
+    }
+    public string Description { get; set; } = string.Empty;
 	public required DateTime DateCreated { get; set; }
 	public required DateTime DateModified { get; set; }
 }
